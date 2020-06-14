16496ba7a9d8016c40ac16e9a168643188847bec
Surbhi Varshney <varshneysurbhi@gmail.com>
2020-06-14 15:07:32 +0530

    Update kang.cpp

--- kang.cpp
+++ kang.cpp
@@ -1,19 +1,24 @@
+#include <iostream>
 
-#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
- 
-    int x1,x2,v1,v2;
+int main(){
+    int x1;
+    int v1;
+    int x2;
+    int v2;
     cin >> x1 >> v1 >> x2 >> v2;
-    while(true){
-        if(x2 > x1 && v2 > v1 || x1 > x2 && v1 > v2 || v1 == v2){
-            cout << "NO" << endl;
-            break;
+    if(v1<=v2)
+        {
+        cout<<"NO";
+    }
+    else{
+        if((x2-x1)%(v2-v1)==0)
+            {
+            cout<<"YES";
         }
-        if((x1 += v1) == (x2 +=v2)){
-            cout << "YES" << endl;
-            break;
+        else{
+            cout<<"NO";
         }
     }
     return 0;
